31bbe18979a788c72350ba096315cd6243ad82f7
Ighomena Daniel Odebala <71289078+dhee-tree@users.noreply.github.com>
2022-04-19 14:06:36 +0100

    Removed password

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -133,7 +133,6 @@ django_heroku.settings(locals())
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "realdheetree@gmail.com"
-EMAIL_HOST_PASSWORD = "Tr33Pword@@#"
 EMAIL_USE_TLS = True
 
 # Default primary key field type
