d3c2a4bd4797d594c0cb2ef1ad16aa27a214d2c1
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2022-04-20 03:21:55 +0100

    updated SECURE PASSWORD

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -143,7 +143,7 @@ django_heroku.settings(locals())
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "realdheetree@gmail.com"
-EMAIL_HOST_PASSWORD = 'Tr33Pword@@#'
+EMAIL_HOST_PASSWORD = 'xhdpbqjppxivmgge'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
