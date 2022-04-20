92ebd70e65771c7e07c34f8de008ea270d09459d
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2022-04-20 01:40:47 +0100

    heroku connected

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-jfu^3zaet3)tb#rve=+evw_7w@x_3l#i2d37fwprtvbiw7@tf)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
