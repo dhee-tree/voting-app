d8074dc6ce7591300f7f546ba7ad6ca8a9632d00
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2022-06-13 08:29:30 +0100

    changed result time

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ function popupFunc() {
 
 // COUNT DOWN
 // Set the date we're counting down to
-var countDownDate = new Date("June 16, 2022 17:00:25").getTime();
+var countDownDate = new Date("June 15, 2022 13:00:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
