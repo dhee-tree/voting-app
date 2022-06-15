e1656d04e6de367f59480b9c89f02dfbf165975f
unknown <ighoodes@gmail.com>
2022-06-15 13:09:31 +0100

    stopping votes

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('code/', views.code, name='code'),
-    path('voting/', views.voting, name='voting'),
+    #path('code/', views.code, name='code'),
+    #path('voting/', views.voting, name='voting'),
     path('results/', views.result, name='results'),
-    path('bd/', views.adminrest, name='reset'),
+    #path('bd/', views.adminrest, name='reset'),
 ]
